cb84c526ee447c9bb139fede2f8ae53d3fc36eb6
ahmetakinsel <ahmet.akinsel@gmail.com>
2022-11-11 15:38:11 +0200

    test

---
+++ app.js
@@ -0,0 +1,14 @@
+//Your code here
+
+class Account {
+  constructor(accname, accno, balance) {
+    this.name = accname;
+    this.accno = accno;
+    this.balance = balance;
+    this.transactions = [];
+  }
+
+  //your code here
+}
+
+module.exports = { Account };
